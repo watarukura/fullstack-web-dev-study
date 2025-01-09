2f44774fa302231fdef8a5221e0ef5a0bcd6b6be
Wataru Kurashima <what.r.j150@gmail.com>
2025-01-09 17:07:58 +0900

    feat: detail page

--- frontend/src/app/inventory/products/page.tsx
+++ frontend/src/app/inventory/products/page.tsx
@@ -1,4 +1,23 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import productsData from "./sample/dummy_products.json";
+import Link from "next/link";
+
+type ProductData = {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+};
+
 export default function Page() {
+  const [data, setData] = useState<Array<ProductData>>([]);
+
+  useEffect(() => {
+    setData(productsData);
+  }, []);
+
   return (
     <>
       <h2>商品一覧</h2>
@@ -10,31 +29,25 @@ export default function Page() {
             <th>商品名</th>
             <th>単価</th>
             <th>説明</th>
+            <th />
+            <th />
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>コットン100％バックリボンティアードワンピース(黒)</td>
-            <td>6900</td>
-            <td>
-              大人の愛らしさを引き立てる、ナチュラルな風合い。リラックスxトレンドを楽しめる、上品なティアードワンピース
-            </td>
-            <td>
-              <button type="button">更新・削除</button>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>ライトストレッチカットソー(ネイビー)</td>
-            <td>2980</td>
-            <td>
-              しなやかな肌触りが心地よい、程よいフィット感のカットソー。ビジネスカジュアルにも普段遣いにも使える、ベーシックなデザイン。
-            </td>
-            <td>
-              <button type="button">更新・削除</button>
-            </td>
-          </tr>
+          {data.map((data: ProductData) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.name}</td>
+              <td>{data.price}</td>
+              <td>{data.description}</td>
+              <td>
+                <Link href={`/inventory/products/${data.id}`}>在庫管理</Link>
+              </td>
+              <td>
+                <button type="button">更新・削除</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
