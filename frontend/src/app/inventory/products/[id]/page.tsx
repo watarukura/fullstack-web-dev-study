db26cb720e092e10da93a47bbde450f9efed892e
Wataru Kurashima <what.r.j150@gmail.com>
2025-01-31 11:36:07 +0900

    feat: chapter5-4-1

--- frontend/src/app/inventory/products/[id]/page.tsx
+++ frontend/src/app/inventory/products/[id]/page.tsx
@@ -1,8 +1,75 @@
 export default function Page() {
   return (
-    <div>
-      <h2>商品在庫管理</h2>
-      <p>商品在庫の一覧を表示する</p>
-    </div>
+      <>
+          <h2>商品在庫管理</h2>
+          <h3>在庫処理</h3>
+          <form>
+              <div>
+                  <label>商品名:</label>
+                  <span>コットン100%バックリボンティアードワンピース(黒)</span>
+              </div>
+              <div>
+                  <label>数量:</label>
+                  <input type="text" />
+              </div>
+              <button>商品を仕入れる</button>
+              <button>商品を卸す</button>
+          </form>
+          <h3>在庫履歴</h3>
+          <table>
+              <thead>
+              <tr>
+                  <th>処理種別</th>
+                  <th>処理日時</th>
+                  <th>単価</th>
+                  <th>数量</th>
+                  <th>価格</th>
+                  <th>在庫数</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                  <td>卸し</td>
+                  <td>2023-04-03 18:54:13</td>
+                  <td>6900</td>
+                  <td>2</td>
+                  <td>13800</td>
+                  <td>390</td>
+              </tr>
+              <tr>
+                  <td>仕入れ</td>
+                  <td>2023-04-03 18:54:13</td>
+                  <td>6900</td>
+                  <td>3</td>
+                  <td>20700</td>
+                  <td>392</td>
+              </tr>
+              <tr>
+                  <td>卸し</td>
+                  <td>2023-04-03 18:54:13</td>
+                  <td>6900</td>
+                  <td>10</td>
+                  <td>69000</td>
+                  <td>389</td>
+              </tr>
+              <tr>
+                  <td>卸し</td>
+                  <td>2023-04-03 18:54:13</td>
+                  <td>6900</td>
+                  <td>10</td>
+                  <td>69000</td>
+                  <td>390</td>
+              </tr>
+              <tr>
+                  <td>仕入れ</td>
+                  <td>2023-04-03 18:54:13</td>
+                  <td>6900</td>
+                  <td>400</td>
+                  <td>2760000</td>
+                  <td>400</td>
+              </tr>
+              </tbody>
+          </table>
+      </>
   );
 }
